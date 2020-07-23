776547370815742be8a1fac80dd989dd6c45ee73
Vitor Pavan <vipavani@hotmail.com>
2020-07-23 10:19:59 +0200

    autoformating

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const DbConnection = async () => {
   try {
-    const connection = await mongoose
-      .connect(process.env.MONGO_URI, {
-        useCreateIndex: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      })
-    console.log(`Connected to Mongo! Database name: "${connection.connections[0].name}"`)
-    return true
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(
+      `Connected to Mongo! Database name: "${connection.connections[0].name}"`
+    );
+    return true;
   } catch (error) {
-    console.error('Error connecting to mongo', error)
-    return false
+    console.error("Error connecting to mongo", error);
+    return false;
   }
-
-}
+};
 
 module.exports = DbConnection;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req ,res) => {
-    
-    
-    res.json({})
+router.get("/", (req, res) => {
+  res.json({});
+});
 
- })
-
- module.exports = router;
+module.exports = router;
