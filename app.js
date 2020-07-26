76aa99e6b9466b0a57f154a9b0e82ff165ed17ba
Vitor Pavan <vipavani@hotmail.com>
2020-07-26 23:59:05 +0200

    folder error fixed

--- app.js
+++ app.js
@@ -224,7 +224,7 @@ app.use(
 
 //Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "./client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
