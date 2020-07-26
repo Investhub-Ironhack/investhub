0a7250697f137e44d68fead4a3e62ebb090deee1
Vitor Pavan <vipavani@hotmail.com>
2020-07-26 17:52:27 +0200

    heroku port ajust

--- app.js
+++ app.js
@@ -220,9 +220,12 @@ app.use(
 
 //app.set("views", path.join(__dirname, "views"));
 //app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "client/build")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+//app.use(express.static(path.join(__dirname, "client/build")));
+//app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
 
 // default value for title local
 app.locals.title = "InvestHub";
