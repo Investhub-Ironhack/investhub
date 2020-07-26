ca30f58e00101b928e94fe95cc2f1194d4f137dd
Vitor Pavan <vipavani@hotmail.com>
2020-07-27 00:24:24 +0200

    heroku static files routes

--- app.js
+++ app.js
@@ -223,10 +223,13 @@ app.use(
 //app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 //Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  //app.use(express.static(path.join(__dirname, "./client/build")));
-  app.use(express.static(path.resolve(__dirname, './client/build')));
-
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 
 // default value for title local
