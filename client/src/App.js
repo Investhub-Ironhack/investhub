07e7afde26aa8d405cdcfaecc77f803df08df265
Leonardo Costa <leo.costa.programming@gmail.com>
2020-07-26 23:32:10 +0200

    created footer Component and fixed footer structure

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,8 @@ import Profile from "./components/Profile";
 import NewArticle from "./components/NewArticle";
 import Stock from "./components/Stock";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import UserPage from "./components/UserPage";
-import githubLogo from "./images/foundation_social-github.png";
-import linkedinLogo from "./images/foundation_social-linkedin.png";
-import instagramLogo from "./images/typcn_social-instagram.png";
-import facebookLogo from "./images/foundation_social-facebook.png";
-import investhubLogo from "./images/investhub-logo.png";
 
 class App extends Component {
   state = {
@@ -29,33 +25,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav class="navbar">
-          <div class="container">
-            <img
-              className="navbar-logo"
-              src={investhubLogo}
-              alt="Investhub's Logo"
-            />
-
-            <div class="menu">
-              <a class="btn" href="/vacancies">
-                Dashboard
-              </a>
-              <a class="btn btn-secundary" href="/auth/logout">
-                Logout
-              </a>
-            </div>
-
-            <div class="menu">
-              <a class="btn" href="/auth/login">
-                Login
-              </a>
-              <a class="btn btn-secundary" href="/auth/signup">
-                Sign Up
-              </a>
-            </div>
-          </div>
-        </nav>
         <Navbar user={this.state.user} setUser={this.setUser} />
 
         <Route exact path="/" render={() => <h1>Home Page</h1>} />
@@ -98,51 +67,7 @@ class App extends Component {
           render={(props) => <Stock setUser={this.setUser} {...props} />}
         />
 
-        <div className="footer">
-          <img
-            className="footer-logo"
-            src={investhubLogo}
-            alt="Investhub's Logo"
-          />
-          <p class="footer-social">Connect with us</p>
-          <div class="footer-social-logos">
-            <a href="http://github.com/">
-              <img
-                className="social-logo"
-                src={githubLogo}
-                alt="Linkedin's Logo"
-              />
-            </a>
-
-            <a href="https://www.instagram.com/">
-              <img
-                className="social-logo"
-                src={instagramLogo}
-                alt="Linkedin's Logo"
-              />
-            </a>
-
-            <a href="http://facebook.com/">
-              <img
-                className="social-logo"
-                src={facebookLogo}
-                alt="Facebook's Logo"
-              />
-            </a>
-
-            <a href="http://linkedin.com/">
-              <img
-                className="social-logo"
-                src={linkedinLogo}
-                alt="Linkedin's Logo"
-              />
-            </a>
-          </div>
-
-          <p class="footer-copyright">
-            2020 © All Rights Reserved. Made in Berlin
-          </p>
-        </div>
+        <Footer user={this.state.user} setUser={this.setUser} />
       </div>
     );
   }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { logout } from "../services/auth";
+import investhubLogo from "../images/investhub-logo.png";
 
 const handleLogout = (props) => {
   logout().then(() => {
@@ -11,6 +12,38 @@ const handleLogout = (props) => {
 function Navbar(props) {
   return (
     <navbar>
+      <nav class="navbar">
+        <div class="container">
+          <img
+            className="navbar-logo"
+            src={investhubLogo}
+            alt="Investhub's Logo"
+          />
+
+          <div class="menu">
+            <Link className="btn btn-secundary" to="/">
+              Home
+            </Link>
+
+            <Link
+              className="btn btn-secundary"
+              to="/"
+              onClick={() => handleLogout(props)}
+            >
+              Logout
+            </Link>
+          </div>
+
+          <div class="menu">
+            <a class="btn" href="/auth/login">
+              Login
+            </a>
+            <a class="btn btn-secundary" href="/auth/signup">
+              Sign Up
+            </a>
+          </div>
+        </div>
+      </nav>
       <Link to="/">Home</Link>
       <Link to="/login">Login</Link>
       <Link to="/signup">Signup</Link>
