7223d4c634188065d3443e5cf0d7f9a816a7da48
Leonardo Costa <leo.costa.programming@gmail.com>
2020-07-27 23:17:28 +0200

    added divs to login card

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -40,10 +40,10 @@ class Login extends Component {
   render() {
     return (
       <>
-        <div class="login-page">
+        <div className="login-page">
           <div>
             <form className="login-form" onSubmit={this.handleSubmit}>
-              <h2>Welcome back!</h2>
+              <h2>Welcome Back!</h2>
               <label htmlFor="username">Username: </label>
               <input
                 type="text"
@@ -64,24 +64,28 @@ class Login extends Component {
               {this.state.message && <div>{this.state.message}</div>}
               <button type="submit">Login</button>
 
-              <div>
-                <a href="http://localhost:5555/api/auth/linkedin">
-                  Login via LinkedIn
-                </a>
-              </div>
-
-              <div>
-                <a href="http://localhost:5555/api/auth/github">
-                  Login via Github
-                </a>
-              </div>
-              <p class="message">
+              <p className="message">
                 Don't have an account?{" "}
                 <a href="/auth/signup">
                   <u>Sign up!</u>
                 </a>
               </p>
             </form>
+          </div>
+
+          <div className="social-signin">
+            <p>Or log in with your social account</p>
+            <div>
+              <a href="http://localhost:5555/api/auth/linkedin">
+                Login via LinkedIn
+              </a>
+            </div>
+
+            <div>
+              <a href="http://localhost:5555/api/auth/github">
+                Login via Github
+              </a>
+            </div>
           </div>
         </div>
       </>
@@ -91,46 +95,44 @@ class Login extends Component {
 
 export default Login;
 
-{
-  /* <div class="login-page">
-  <div>
-    <form class="login-form" action="/auth/login" method="POST">
-      <h2>Welcome back!</h2>
-      <input type="text" name="username" placeholder="Username" required /><br>
-      <input type="password" name="password" placeholder="Password" required /><br>
-      <button>Login</button>
-      {{#if errorMessage}}
-      <div class="error-message">{{errorMessage}}</div>
-      {{/if}}
-      <p class="message">Don't have an account? <a href="/auth/signup"><u>Sign up!</u></a></p>
-    </form>
-  </div>
-  <div class="social-signin">
-    <p>or log in with your social account.</p>
-    <div>
-      <a href="/auth/github">
-        <img src="/images/github_color.png" alt="public/images/github_color.png">
-      </a>
-      <a href="/auth/github">Login via Github</a>
-    </div>
-    <div>
-      <a href="/auth/linkedin">
-        <img src="/images/linkedIn_color.png" alt="public/images/linkedIn_color.png">
-      </a>
-      <a href="/auth/linkedin">Login via LinkedIn</a>
-    </div>
-    <div>
-      <a href="/auth/google">
-        <img src="/images/google_color.png" alt="public/images/google_color.png">
-      </a>
-      <a href="/auth/google">Login via Google</a>
-    </div>
-    <div>
-      <a href="/auth/xing">
-        <img src="/images/xing_color.png" alt="public/images/xing_color.png">
-      </a>
-      <a href="/auth/xing">Login via Xing</a>
-    </div>
-  </div>
-</div> */
-}
+// <div class="login-page">
+//   <div>
+//     <form class="login-form" action="/auth/login" method="POST">
+//       <h2>Welcome back!</h2>
+//       <input type="text" name="username" placeholder="Username" required /><br>
+//       <input type="password" name="password" placeholder="Password" required /><br>
+//       <button>Login</button>
+//       {{#if errorMessage}}
+//       <div class="error-message">{{errorMessage}}</div>
+//       {{/if}}
+//       <p class="message">Don't have an account? <a href="/auth/signup"><u>Sign up!</u></a></p>
+//     </form>
+//   </div>
+//   <div class="social-signin">
+//     <p>or log in with your social account.</p>
+//     <div>
+//       <a href="/auth/github">
+//         <img src="/images/github_color.png" alt="public/images/github_color.png">
+//       </a>
+//       <a href="/auth/github">Login via Github</a>
+//     </div>
+//     <div>
+//       <a href="/auth/linkedin">
+//         <img src="/images/linkedIn_color.png" alt="public/images/linkedIn_color.png">
+//       </a>
+//       <a href="/auth/linkedin">Login via LinkedIn</a>
+//     </div>
+//     <div>
+//       <a href="/auth/google">
+//         <img src="/images/google_color.png" alt="public/images/google_color.png">
+//       </a>
+//       <a href="/auth/google">Login via Google</a>
+//     </div>
+//     <div>
+//       <a href="/auth/xing">
+//         <img src="/images/xing_color.png" alt="public/images/xing_color.png">
+//       </a>
+//       <a href="/auth/xing">Login via Xing</a>
+//     </div>
+//   </div>
+// </div>
