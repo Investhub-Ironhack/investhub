a01c863849d0f7091d0571b6cee4f2ca8579e8a5
Vitor Pavan <vipavani@hotmail.com>
2020-07-29 21:56:17 +0200

    npm run build

---
+++ client/build/precache-manifest.1454de41657338dbb0b2f11c71de07ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cbc769284108d697845b3e7afdae759",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71f75c88ac335c183a12",
+    "url": "/static/css/main.d877c29c.chunk.css"
+  },
+  {
+    "revision": "140b44927b43429ff7d9",
+    "url": "/static/js/2.89d1bbfc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.89d1bbfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71f75c88ac335c183a12",
+    "url": "/static/js/main.1017084b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
