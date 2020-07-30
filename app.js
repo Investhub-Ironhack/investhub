36e16a9f646f844d384719dc075cf817d824597b
Vitor Pavan <vipavani@hotmail.com>
2020-07-30 10:12:48 +0200

    app.get

--- app.js
+++ app.js
@@ -228,10 +228,10 @@ app.use(
 if (process.env.NODE_ENV === "production") {
   console.log(`happenind?`);
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
   // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/client/build", "index.html"));
   });
 }
 
