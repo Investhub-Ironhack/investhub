72f33f9176bcea2922f376a86d1a18cd487806af
Eliott Fournet <eliott.frnt@gmail.com>
2020-07-30 18:29:21 +0100

    npm run build

---
+++ client/build/precache-manifest.3467d8e25d3180cb361cc9e85b7eed18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58234b9a02918c9112e165b50e892ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7b688d37a19ac98da06",
+    "url": "/static/css/main.88e9523c.chunk.css"
+  },
+  {
+    "revision": "61367f57202cc9e4f8cb",
+    "url": "/static/js/2.271ef8e8.chunk.js"
+  },
+  {
+    "revision": "5e5c5a8ef7ce9bcac22b24b05c556eb1",
+    "url": "/static/js/2.271ef8e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b688d37a19ac98da06",
+    "url": "/static/js/main.efad94e5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
